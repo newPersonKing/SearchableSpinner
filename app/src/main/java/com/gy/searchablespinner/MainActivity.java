204a7d682de489ced176d53db01dae1e8c96f858
guoyong <guoyong@emcc.net.com>
2018-08-03 14:47:02 +0800

    spinner

---
+++ app/src/main/java/com/gy/searchablespinner/MainActivity.java
@@ -0,0 +1,31 @@
+package com.gy.searchablespinner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.gy.searchablespinner.widget.SearchableSpinner;
+
+public class MainActivity extends AppCompatActivity implements SearchableSpinner.OnSelectionChangeListener {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SearchableSpinner spinner1 = (SearchableSpinner) findViewById(R.id.search1);
+        spinner1.setOnSelectionChangeListener(this);
+
+        SearchableSpinner spinner2 = (SearchableSpinner) findViewById(R.id.search2);
+        String [] list = {"ONE", "TWO"};
+        spinner2.setList(list);
+
+        SearchableSpinner spinner3 = (SearchableSpinner) findViewById(R.id.search3);
+        spinner3.setList(getResources().getStringArray(R.array.all_languages));
+    }
+
+    @Override
+    public void onSelectionChanged(String selection) {
+        Toast.makeText(this, selection + " selected", Toast.LENGTH_SHORT).show();
+    }
+}
